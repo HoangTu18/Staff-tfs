b62d6c1f4cdad194e719380b8bdd2708aa0c1a4a
danhpq176 <danhpq176@github.com>
2023-03-07 23:20:00 +0700

    OrderPage update

--- src/page/OrderPage/index.jsx
+++ src/page/OrderPage/index.jsx
@@ -1,15 +1,71 @@
 import Box from "@mui/material/Box";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus,faCircleExclamation } from '@fortawesome/free-solid-svg-icons'
+import "./index.css";
+const count = 3;
 const OrderPage = () => {
   return (
     <Box
       sx={{
         width: "100%",
-        backgroundColor: "primary.dark",
+        
       }}
     >
-      <div>
-        <h1>Order page</h1>
+      <div className="order-page">
+        <div className="order-title">
+          <h2>Đơn đặt hàng</h2>
+          <FontAwesomeIcon icon={faPlus} />
+        </div>
+        <div className="order-calender">
+          <input type={"date"} className="date" />
+        </div>
+        <h2 className="order-sum">Tổng đơn hàng: {count}</h2>
+        <div className="order-list">
+          <div className="order-item">
+            <div className="left">
+              <h3 className="orderID">#17</h3>
+              <h3 className="order-customer">Phạm Hoàng Tú</h3>
+              <div className="order-products">{count} sản phẩm</div>
+            </div>
+            <div className="right">
+              <h3 className="price">400.000 VND</h3>
+              <div className="confirmnative">
+              <FontAwesomeIcon icon={faCircleExclamation} />
+              <span>Chờ xác nhận</span>
+              </div>
+            </div>
+          </div>
+          <div className="order-item">
+            <div className="left">
+              <h3 className="orderID">#17</h3>
+              <h3 className="order-customer">Phạm Hoàng Tú</h3>
+              <div className="order-products">{count} sản phẩm</div>
+            </div>
+            <div className="right">
+              <h3 className="price">400.000 VND</h3>
+              <div className="confirmnative">
+              <FontAwesomeIcon icon={faCircleExclamation} />
+              <span>Chờ xác nhận</span>
+              </div>
+            </div>
+          </div>
+          <div className="order-item">
+            <div className="left">
+              <h3 className="orderID">#17</h3>
+              <h3 className="order-customer">Phạm Hoàng Tú</h3>
+              <div className="order-products">{count} sản phẩm</div>
+            </div>
+            <div className="right">
+              <h3 className="price">400.000 VND</h3>
+              <div className="confirmnative">
+              <FontAwesomeIcon icon={faCircleExclamation} />
+              <span>Chờ xác nhận</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+      
     </Box>
   );
 };
